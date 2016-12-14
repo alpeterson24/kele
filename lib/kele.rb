d678ad714c862301280201f19d6adbd0feee19ad
Al Peterson <alpeterson@Als-MacBook-Pro.local>
2016-12-13 23:10:19 -0600

    checkpoint_3

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,20 +3,30 @@ require 'httparty'
 
 module Kele
      class Kele
+         require 'json'
          include HTTParty
          base_uri "https://www.bloc.io/api/v1"
 
          def initialize(email, password)
              @auth = {email: email, password: password}
 
-             post_response = self.class.post(
-                 "/sessions",
-                 body: @auth
-             )
+             post_response = self.class.post( "/sessions", body: @auth )
 
-             @auth_token = post_response.body["auth_token"]
+             @auth_token = post_response["auth_token"]
 
              raise "Invalid Credentials" if (!@auth_token)
+         end
+
+         def get_me
+             response = self.class.get( "/users/me", headers: {"authorization" => @auth_token} )
+             parse_user_data(response.body)
+         end
+
+        private
+
+         def parse_user_data(resp)
+             user_hash = JSON.parse(resp)
+             puts user_hash
          end
       end
  end
