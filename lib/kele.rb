51ae193fdbcc89029233690a996c4718848150ce
Al Peterson <alpeterson@Als-MacBook-Pro.local>
2016-12-16 12:03:43 -0600

    checkpoint 6

--- lib/kele.rb
+++ lib/kele.rb
@@ -35,6 +35,27 @@ module Kele
              mentor_availability.map {|time| time if time ["booked"] == nil}
          end
 
+         def get_messages(*page)
+             raise ArgumentError if page.length > 1
+             response = self.class.get(
+                 "/message_threads",
+                 headers: {"authorization" => @auth_token},
+             )
+             parser(response.body)
+         end
+
+         def create_message(user_id, recipient_id, options={})
+             msg_data = {user_id: user_id, recipient_id: recipient_id, subject: options[:subject], "stripped-text" => options[:body]}
+             msg_data[:token] = options[:token] if options[:token]
+
+             msg_post_response = self.class.post(
+                 "/messages",
+                 {headers: {"authorization" => @auth_token},
+                 body: msg_data}
+             )
+             parser(msg_post_response.body)
+         end
+
         private
 
          def parser(data)
